c52506653634f033fedf124bc33529914b8dfcca
iFrodo <igor@azinet.ru>
2023-10-24 16:24:01 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { Box, Text, Button, Flex } from '@chakra-ui/react'
+import { useState } from 'react';
 import './App.css';
 
+function Numbers(props) {
+  const nums = Array.from(Array(10).keys()).map(
+    number => {
+      return <Button onClick={(e) => {
+        if(props.data != 0) props.onClick(props.data + e.target.innerHTML)
+        else props.onClick(e.target.innerHTML)
+      }}
+        key={number} w={'40px'} h={'40px'} margin={'4px'}>{number}</Button>
+    }
+  )
+  return (
+    <Box display={'flex'} flexWrap={'wrap'} w={'15%'}>{nums}</Box>
+  )
+}
+
+function CountButton (props) {
+return (
+  <Button onClick = {()=>{props.onClick(props.data + props.expression)}}>
+    {props.expression}
+
+  </Button>
+)
+}
+
+
+
+
 function App() {
+
+  const [counts, setCounts] = useState(0);
+  const [result, setResult] = useState('');
+
+  function applyExpression (countedNumber) {
+    setCounts(countedNumber)
+    setResult(eval(counts))
+  
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box display={'flex'} justifyContent={'center'} flexDirection={'column'}  alignItems={'center'} h={"100vh"}>
+        <Box display={'flex'} justifyContent={'center'} gap={'5px'} flexDirection={'column'}  alignItems={'center'} w={'200px'}>
+          <Box display={'flex'} w={'100%'} justifyContent={'space-between'}>
+            <Text display={'flex'} justifyContent={'start'} alignItems={'center'}
+              bg={'gray.50'} w={'100%'} h={'38px'} px={'4px'} borderRadius={'8px'}>
+              {counts}
+            </Text>
+            <Text w={'fit-content'} h={'38px'} textColor={'tomato'}>
+              {result}
+            </Text>
+          </Box>
+        </Box>
+      
+        <Numbers data={counts} onClick={setCounts} />
+        <Box display={'flex'}>
+          <CountButton data={counts} expression={'+'} onClick={applyExpression}/>
+        </Box>
+      
+      </Box>
     </div>
   );
 }
